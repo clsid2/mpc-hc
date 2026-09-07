0ac53ff78333711751cbcd73f9959b94feea62ee
clsid2 <4704996+clsid2@users.noreply.github.com>
2026-09-08 01:53:35 +0200

    .

--- src/mpc-hc/AppSettings.cpp
+++ src/mpc-hc/AppSettings.cpp
@@ -3944,11 +3944,6 @@ void CAppSettings::MigrateSettings()
             }
         }
         [[fallthrough]];
-        case 1: {
-            // Internal decoding of WMV 1/2/3 is now disabled by default so we reinitialize its value
-            pApp->WriteProfileInt(IDS_R_INTERNAL_FILTERS, _T("TRA_WMV"), FALSE);
-        }
-        [[fallthrough]];
         case 2: {
             const CString section(_T("Settings"));
             if (pApp->HasProfileEntry(section, _T("FullScreenCtrls")) &&
@@ -4057,10 +4052,6 @@ void CAppSettings::MigrateSettings()
             VERIFY(pApp->WriteProfileInt(IDS_R_SETTINGS, IDS_RS_DISABLE_SUBTITLE_ANIMATION, bDisableSubtitleAnimation));
         }
         [[fallthrough]];
-        case 5:
-            copyInt(IDS_R_INTERNAL_FILTERS, _T("SRC_DTSAC3"), IDS_R_INTERNAL_FILTERS, _T("SRC_DTS"));
-            copyInt(IDS_R_INTERNAL_FILTERS, _T("SRC_DTSAC3"), IDS_R_INTERNAL_FILTERS, _T("SRC_AC3"));
-        [[fallthrough]];
         case 6: {
             SubtitleRenderer subrenderer = SubtitleRenderer::INTERNAL;
             if (!pApp->GetProfileInt(IDS_R_SETTINGS, _T("AutoloadSubtitles"), TRUE)) {
@@ -4082,7 +4073,7 @@ void CAppSettings::MigrateSettings()
             // Update the settings after the removal of DirectX 7 renderers
             switch (pApp->GetProfileInt(IDS_R_SETTINGS, IDS_RS_DSVIDEORENDERERTYPE, VIDRNDT_DS_VMR7)) {
                 case 3: // VIDRNDT_DS_VMR7WINDOWED
-                    VERIFY(pApp->WriteProfileInt(IDS_R_SETTINGS, IDS_RS_DSVIDEORENDERERTYPE, VIDRNDT_DS_VMR9WINDOWED));
+                    VERIFY(pApp->WriteProfileInt(IDS_R_SETTINGS, IDS_RS_DSVIDEORENDERERTYPE, VIDRNDT_DS_VMR7));
                     break;
                 case 5: // VIDRNDT_DS_VMR7RENDERLESS
                     VERIFY(pApp->WriteProfileInt(IDS_R_SETTINGS, IDS_RS_DSVIDEORENDERERTYPE, VIDRNDT_DS_VMR9RENDERLESS));
